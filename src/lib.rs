0e71da49624832fd2f369d94720988f1cef00190
Xuan Yan <xuanyan@xuanyan.ws>
2021-02-16 03:44:16 +0800

    reusable solver

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod number;
 mod number_theory;
 mod progressive_solver;
 mod quadratic;
+mod reusable_solver;
 mod solver;
 mod wasm;
 
@@ -11,4 +12,5 @@ pub use expression::Expression;
 pub use number::Number;
 pub use progressive_solver::ProgressiveSolver;
 pub use quadratic::{IntegralQuadratic, RationalQuadratic, PRIMES};
+pub use reusable_solver::ReusableSolver;
 pub use solver::{Limits, Solver};
---
+++ src/reusable_solver.rs
@@ -0,0 +1,166 @@
+use super::{Limits, Solver};
+use crate::{Expression, Number, RationalQuadratic};
+use num::rational::Rational64;
+use std::rc::Rc;
+
+enum ReusableSearchState {
+    None,
+    Integral,
+    Rational,
+    RationalQuadratic,
+    Finished,
+}
+
+pub struct ReusableSolver {
+    target: i64,
+    integral_solver: Solver<i64>,
+    rational_solver: Solver<Rational64>,
+    rational_quadratic_solver: Solver<RationalQuadratic>,
+    depth_searched: usize,
+    search_state: ReusableSearchState,
+}
+
+impl ReusableSolver {
+    pub fn new(
+        n: i64,
+        integral_limits: Limits,
+        rational_limits: Limits,
+        quadratic_limits: Limits,
+    ) -> Self {
+        Self {
+            target: 0,
+            integral_solver: Solver::<i64>::new_progressive(n, integral_limits),
+            rational_solver: Solver::<Rational64>::new_progressive(n, rational_limits),
+            rational_quadratic_solver: Solver::<RationalQuadratic>::new_progressive(
+                n,
+                quadratic_limits,
+            ),
+            depth_searched: 0,
+            search_state: ReusableSearchState::None,
+        }
+    }
+
+    pub fn solve(
+        &mut self,
+        target: i64,
+        max_depth: Option<usize>,
+    ) -> Option<(Rc<Expression>, usize)> {
+        self.target = target;
+        if let Some((expression, digits)) = self.get_solution(&self.target) {
+            return if max_depth.unwrap_or(usize::MAX) >= *digits {
+                Some((expression.clone(), *digits))
+            } else {
+                None
+            };
+        }
+        for digits in self.depth_searched + 1..=max_depth.unwrap_or(usize::MAX) {
+            if self.search(digits) {
+                return Some(self.get_solution(&self.target)?.clone());
+            }
+        }
+        None
+    }
+
+    pub fn get_solution(&self, x: &i64) -> Option<&(Rc<Expression>, usize)> {
+        self.integral_solver
+            .get_solution(x)
+            .or_else(|| {
+                self.rational_solver
+                    .get_solution(&Rational64::from_integer(*x))
+            })
+            .or_else(|| {
+                self.rational_quadratic_solver
+                    .get_solution(&RationalQuadratic::from_int(*x))
+            })
+    }
+
+    fn search(&mut self, digits: usize) -> bool {
+        match self.search_state {
+            ReusableSearchState::None => {
+                self.search_state = ReusableSearchState::Integral;
+            }
+            _ => {}
+        }
+        match self.search_state {
+            ReusableSearchState::Integral => {
+                if self
+                    .integral_solver
+                    .solve(self.target, Some(digits))
+                    .is_some()
+                {
+                    return true;
+                }
+                for (x, expression, _) in self.integral_solver.new_numbers() {
+                    self.rational_solver
+                        .try_insert(Rational64::from_integer(*x), digits, || expression.clone());
+                    self.rational_quadratic_solver.try_insert(
+                        RationalQuadratic::from_int(*x),
+                        digits,
+                        || expression.clone(),
+                    );
+                }
+                self.clear_new_numbers();
+                self.search_state = ReusableSearchState::Rational;
+            }
+            _ => {}
+        }
+        match self.search_state {
+            ReusableSearchState::Rational => {
+                if self
+                    .rational_solver
+                    .solve(Rational64::from_integer(self.target), Some(digits))
+                    .is_some()
+                {
+                    return true;
+                }
+                for (x, expression, _) in self.rational_solver.new_numbers() {
+                    if let Some(x_int) = x.to_int() {
+                        self.integral_solver
+                            .try_insert(x_int, digits, || expression.clone());
+                    }
+                    self.rational_quadratic_solver.try_insert(
+                        RationalQuadratic::from_rational(*x),
+                        digits,
+                        || expression.clone(),
+                    );
+                }
+                self.clear_new_numbers();
+                self.search_state = ReusableSearchState::RationalQuadratic;
+            }
+            _ => {}
+        }
+        match self.search_state {
+            ReusableSearchState::RationalQuadratic => {
+                if self
+                    .rational_quadratic_solver
+                    .solve(RationalQuadratic::from_int(self.target), Some(digits))
+                    .is_some()
+                {
+                    return true;
+                }
+                for (x, expression, _) in self.rational_quadratic_solver.new_numbers() {
+                    if let Some(x_int) = x.to_int() {
+                        self.integral_solver
+                            .try_insert(x_int, digits, || expression.clone());
+                    }
+                    if x.is_rational() {
+                        self.rational_solver
+                            .try_insert(x.rational_part(), digits, || expression.clone());
+                    }
+                }
+                self.clear_new_numbers();
+                self.search_state = ReusableSearchState::Finished;
+            }
+            _ => {}
+        }
+        self.depth_searched = digits;
+        self.search_state = ReusableSearchState::None;
+        false
+    }
+
+    fn clear_new_numbers(&mut self) {
+        self.integral_solver.clear_new_numbers();
+        self.rational_solver.clear_new_numbers();
+        self.rational_quadratic_solver.clear_new_numbers();
+    }
+}
